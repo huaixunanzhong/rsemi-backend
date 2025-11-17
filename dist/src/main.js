0127afc4cff9ee570798bb9f508d9f312d7ba7ab
Guolong Chen <chenguolong@mycar.direct>
2025-11-17 17:26:22 +0800

    Initial Commit

--- dist/src/main.js
+++ dist/src/main.js
@@ -3,15 +3,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const response_interceptor_1 = require("./common/interceptors/response.interceptor");
+const swagger_1 = require("@nestjs/swagger");
+function useSwagger(app) {
+    const options = new swagger_1.DocumentBuilder()
+        .setTitle("Swagger API")
+        .setDescription("The Swagger API Description")
+        .setVersion("1.0")
+        .addTag("Swagger")
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, options);
+    swagger_1.SwaggerModule.setup("api", app, document);
+}
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { abortOnError: false });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+        abortOnError: false,
+    });
     app.enableCors({
-        origin: 'http://localhost:3000',
-        methods: 'GET,POST,PUT,DELETE',
+        origin: "http://localhost:3000",
+        methods: "GET,POST,PUT,DELETE",
         credentials: false,
     });
     app.useGlobalInterceptors(new response_interceptor_1.ResponseInterceptor());
-    await app.listen(process.env.PORT || 3001);
+    useSwagger(app);
+    const port = process.env.PORT || 3001;
+    await app.listen(port);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,39 @@
-import { NestFactory } from '@nestjs/core';
-import type { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
-import {ResponseInterceptor} from "./common/interceptors/response.interceptor";
+import { NestFactory } from "@nestjs/core";
+import type { NestExpressApplication } from "@nestjs/platform-express";
+import { AppModule } from "./app.module";
+import { ResponseInterceptor } from "./common/interceptors/response.interceptor";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+// 开启swagger api
+function useSwagger(app: NestExpressApplication) {
+  const options = new DocumentBuilder()
+    .setTitle("Swagger API")
+    .setDescription("The Swagger API Description")
+    .setVersion("1.0")
+    .addTag("Swagger")
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api", app, document);
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule,{abortOnError:false});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    abortOnError: false,
+  });
 
-    app.enableCors({
-        origin: 'http://localhost:3000', // 只允许你的前端访问
-        methods: 'GET,POST,PUT,DELETE',
-        credentials: false,
-    });
+  app.enableCors({
+    origin: "http://localhost:3000", // 只允许你的前端访问
+    methods: "GET,POST,PUT,DELETE",
+    credentials: false,
+  });
 
-    app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
-  await app.listen(process.env.PORT || 3001);
+  // 使用swagger生成API文档
+  useSwagger(app);
+
+  // 服务监听
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
 }
 bootstrap();
